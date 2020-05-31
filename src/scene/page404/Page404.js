e32363676b6b718e55300e974aca4566884871fa
amanjotsinghdhunna <amanjot.singh@successive.tech>
2020-06-01 01:34:35 +0530

    wip

--- src/scene/page404/Page404.js
+++ src/scene/page404/Page404.js
@@ -1,24 +1,17 @@
 import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import logo from './page404-2.gif'
 import "./css.css";
 
+// https://codepen.io/amanjotsuccessive/pen/LYpvYMj
 export default function Page404() {
-    // https://codepen.io/amanjotsuccessive/pen/LYpvYMj
     return (
-        <div class="ag-page-404">
-            <div class="ag-toaster-wrap">
-                <div class="ag-toaster">
-                    <div class="ag-toaster_back"></div>
-                    <div class="ag-toaster_front">
-                        <div class="js-toaster_lever ag-toaster_lever"></div>
-                    </div>
-                    <div class="ag-toaster_toast-handler">
-                        <div class="ag-toaster_shadow"></div>
-                        <div class="js-toaster_toast ag-toaster_toast js-ag-hide"></div>
-                    </div>
-                </div>
-
-                <canvas id="canvas-404" class="ag-canvas-404"></canvas>
-            </div>
-        </div>
+        <Container>
+            <Row>
+                <Col className='d-flex justify-content-center align-items-center w-100'>
+                    <img className src={logo} alt='animated' />
+                </Col>
+            </Row>
+        </Container>
     )
 }
